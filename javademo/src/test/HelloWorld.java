5b357574727ed57613bcb06238477b3efc3f73bc
admin <admin@qq.com>
2020-11-18 11:14:09 +0800

    我的第二次提交代码

--- javademo/src/test/HelloWorld.java
+++ javademo/src/test/HelloWorld.java
@@ -5,7 +5,15 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		//打印一个helloworld!!!
 		System.out.println("hello world!!!");
-
+	}
+	
+	
+	
+	/**
+	 * sayhi的方法
+	 */
+	public void sayhi(){
+		System.out.println("sayhi的方法......!");
 	}
 
 }
